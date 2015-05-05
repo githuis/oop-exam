eb2d4121323bf90a611aef7fe78b9f71e53211a8
githuis <zhex@live.dk>
2015-05-05 13:52:27 +0200

    Added User

---
+++ StregSystem/StregSystem/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StregSystem
+{
+    class User
+    {
+        private int _userID;
+        private string _firstname;
+        private string _lastname;
+        private string _username;
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        
+        public string Username
+        {
+            get { return _username; }
+        }
+        
+        public string Lastname
+        {
+            get { return _lastname; }
+        }
+        
+        public string Firstname
+        {
+            get { return _firstname; }
+        }
+
+        public int UserID
+        {
+            get { return _userID; }
+        }
+        
+    }
+}
